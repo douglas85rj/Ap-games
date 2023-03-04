622ec200823f247d2d4950e349f6d17952a4d4b6
douglas85rj <douglas85rj@gmail.com>
2023-03-04 18:46:39 -0300

    Array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,41 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Section from './components/Section';
+import ListItem from './components/ListItem';
+
+
+
+const gamesListData = [
+
+  { url:"https://www.twitch.tv/directory/game/League%20of%20Legends", 
+  imageUrl:"https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg"  ,
+  alt:"Imagem do jogo League of Legends" ,
+
+},
+  
+  {
+  url:"https://www.twitch.tv/directory/game/VALORANT",
+      imageUrl:"https://static-cdn.jtvnw.net/ttv-boxart/516575-188x250.jpg",
+       alt:"Imagem do jogo Valorant",
+
+},
+
+  
+  {
+  url:"https://www.twitch.tv/directory/game/Minecraft",
+      imageUrl:"https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-188x250.jpg" ,
+      alt:"Imagem do jogo Minecraft",
+ 
+  },
+
+  
+  {
+  url:"https://www.twitch.tv/directory/game/Teamfight%20Tactics",
+      imageUrl:"https://static-cdn.jtvnw.net/ttv-boxart/513143-188x250.jpg",
+       alt:"Imagem do jogo TFT",
+  
+},
+]
 
 function App() {
   return (
@@ -11,8 +46,26 @@ function App() {
 
 <main>
   <Section title="Meus Jogos" 
-  subtitle="Os games que eu mais curto jogar!" />
+  subtitle="Os games que eu mais curto jogar!">
 
+    {
+
+
+  gamesListData.map(function(item) {
+return(
+<ListItem
+url={item.url}
+imageUrl={item.imageUrl}
+alt={item.alt}
+/>
+)
+
+})
+
+    }
+
+
+</Section>
     <Section title="Canais e Streamers" 
   subtitle="Lista de canais e transmissões que eu não perco" />
 </main>
