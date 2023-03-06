3c0829a675a3811cd90391625f218a43c35a6da0
douglas85rj <douglas85rj@gmail.com>
2023-03-06 09:27:12 -0300

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,30 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import Section from "../components/Section";
 import styled from "styled-components";
 
+const Footer = () => {
+  return (
+    <div>
+      <Button as="a" href="/">
+        Home
+      </Button>
+      <Button as="a" href="/contato">
+        Contato
+      </Button>
+     
+    </div>
+  );
+};
 
-
-const Footer =() => {
-
-    return (
-        <nav>
-<Link to="/">Home</Link><br/>
-<Link to="/contato">Contato</Link>
-        </nav>
-    )
-}
-
-
-
+const Button = styled.button`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  display: block;
+`;
 
 export default Footer;
