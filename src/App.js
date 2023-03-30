2d3c8769dae6da94215ba20e2ea2844e05756699
douglas85rj <douglas85rj@gmail.com>
2023-03-30 20:36:17 -0300

    remove comented lines

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-//import Section from './components/Section';
-//import ListItem from './components/ListItem';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 
