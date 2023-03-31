f65acaa3c228b0d3b736a869306b9a90796a1b05
douglas85rj <douglas85rj@gmail.com>
2023-03-30 22:08:16 -0300

    Layout_footer_form_ajustado

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,46 @@
 import { Link } from "react-router-dom";
 import Section from "../components/Section";
 import styled from "styled-components";
+import "../App.css";
+
 
 const Footer = () => {
   return (
-    <div>
+    
+<div>
+            <ul>
+      <li>
       <Button as="a" href="/">
         Home
       </Button>
+      </li>
+      <li>
       <Button as="a" href="/contato">
         Contato
       </Button>
-     
-    </div>
+     </li>
+    </ul>
+</div>
+    
   );
 };
 
 const Button = styled.button`
-  display: inline-block;
+  display: inline;
   color: palevioletred;
   font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  margin: 9px;
+  padding: 15px;
+  border: 6px solid palevioletred;
   border-radius: 3px;
-  display: block;
+display:block;
+text-align: center;
+margin-left: auto;
+  margin-right: auto;
+width:30%;
+text-decoration:none;
+  
+  
 `;
 
 export default Footer;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,91 +1,124 @@
-import { render } from "@testing-library/react"
-import React from "react"
-import styled from "styled-components"
-import Section from "./Section";
-import '../App.css';
-
-
-
+import { render } from "@testing-library/react";
+import React from "react";
+import styled from "styled-components";
+import "../App.css";
 
 export default function Form() {
+  return (
+    
+    <div>
+      <form
+        className="conato"
+        method="post"
+        action="https://getform.io/f/97332049-19ab-4e2d-bd51-90d0accb9b30"
+      >
+        <div className="row">
+          <Input
+            type="text"
+            name="fullname"
+            id="nome"
+            placeholder="Nome"
+            required
+            className="field"
+            minLength={2}
+            maxLength={36}
+          />
+        </div>
+        <div className="row">
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email@seuemail.com"
+            required
+            className="field"
+          />
+        </div>
+        <div className="row">
+          <Input
+            type="tel"
+            name="tel"
+            id="tel"
+            placeholder="Telefone (21) 99999-9999"
+            className="field"
+            pattern="\([0-9][0-9]\) 9?([0-9]{4}-[0-9]{4})"
+          />
+        </div>
+        <div className="row">
+          <TextArea
+            name="mensagem"
+            id="mensagem"
+            required
+            rows={3}
+            className="field"
+            minLength={12}
+            maxLength={256}
+            placeholder="Seu texto com no mínimo 12 e no máximo 256 caracteres."
+          />
+        </div>
+        <Button>Enivar</Button>
+      </form>
+    </div>
+    
+  );
+}
 
-    
-    return (
+const Input = styled.input.attrs((props) => ({
+  // we can define static props
+  type: "text",
 
-   
-        
-    <div className="form-contato">
-        
+  // or we can define dynamic ones
+  size: props.size || "1em",
+}))`
+  width: 70%;  
+  color: palevioletred;
+  font-size: 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  padding: 12px 20px;
+  margin: 10px 0;
+  margin-left: auto;
+  margin-right: auto;
+  display: auto;
 
-   
-<form className="conato" method="post" action="https://getform.io/f/97332049-19ab-4e2d-bd51-90d0accb9b30"> 
-<div className="row">
-<Input type="text" name="fullname" id="nome" placeholder="Nome" required className="field" minLength={2} maxLength={36}/>
-</div>
-<div className="row">
-<Input type="email" name="email" id="email" placeholder="email@seuemail.com" required className="field" />
-</div>
-<div className="row">
-<Input type="tel" name="tel" id="tel" placeholder="Telefone (21) 99999-9999" className="field" pattern="\([0-9][0-9]\) 9?([0-9]{4}-[0-9]{4})"/>
-</div>
-<div className="row">
-<TextArea name="mensagem" id="mensagem" required rows={3} className="field" minLength={12} maxLength={256} placeholder="Seu texto com no mínimo 12 e no máximo 256 caracteres."/>
-</div>
-<Button>Enivar</Button> 
-</form>
- 
-</div>
-         
-)}
 
-const Input = styled.input.attrs(props => ({
-        // we can define static props
-        type: "text",
-      
-        // or we can define dynamic ones
-        size: props.size || "1em",
-      }))`
-      width: 100%;
-        color: palevioletred;
-        font-size: 1em;
-        border: 2px solid palevioletred;
-        border-radius: 3px;
-      
-        /* here we use the dynamically computed prop */
-        margin: ${props => props.size};
-        padding: ${props => props.size};
-      `;
-    
- const TextArea =styled.input.attrs(props => ({
-
-    type: "text",    
-    
-    size: props.size || "1em",
-  }))`
-  width: 100%;
-    color: palevioletred;
-    font-size: 1em;
-    border: 2px solid palevioletred;
-    border-radius: 3px;
-    
-    
-    
-    /* here we use the dynamically computed prop */
-    margin: ${props => props.size};
-    padding: ${props => props.size};
-    
+  /* here we use the dynamically computed prop */
+  margin: ${(props) => props.size};
+  padding: ${(props) => props.size};
 `;
 
+const TextArea = styled.input.attrs((props) => ({
+  type: "text",
+
+  size: props.size || "1em",
+}))`
+  width: 70%;
+  color: palevioletred;
+  font-size: 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  margin: auto;
+  display:inline;
+  margin-left: auto;
+  margin-right: auto;
+ display:auto;
+
+
+  /* here we use the dynamically computed prop */
+  margin: ${(props) => props.size};
+  padding: ${(props) => props.size};
+`;
 
 const Button = styled.button`
   /* Adapt the colors based on primary prop */
-  background: ${props => props.primary ? "palevioletred" : "white"};
-  color: ${props => props.primary ? "white" : "palevioletred"};
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
 
   font-size: 1em;
-  margin: 1em;
+  margin: 4em;
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
   border-radius: 3px;
-`;
+  
 
+`;
