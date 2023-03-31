25986beab8c66cb91f928966f2d31fc5ef91f1d2
douglas85rj <douglas85rj@gmail.com>
2023-03-31 12:28:46 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,46 @@
 import { Link } from "react-router-dom";
-import Section from "../components/Section";
 import styled from "styled-components";
 import "../App.css";
 
-
 const Footer = () => {
   return (
-    
-<div>
-            <ul>
-      <li>
-      <Button as="a" href="/">
-        Home
-      </Button>
-      </li>
-      <li>
-      <Button as="a" href="/contato">
-        Contato
-      </Button>
-     </li>
-    </ul>
-</div>
-    
+    <div>
+      <ul>
+        <li>
+          <Button as="a" href="/">
+            Home
+          </Button>
+        </li>
+        <li>
+          <Button as="a" href="/contato">
+            Contato
+          </Button>
+        </li>
+      </ul>
+    </div>
   );
-
-
-  
 };
 
 const Button = styled.button`
   display: inline;
+
   color: palevioletred;
+
   font-size: 1em;
   margin: 9px;
   padding: 15px;
-  border: 6px solid palevioletred;
+
   border-radius: 3px;
-display:block;
-text-align: center;
-margin-left: auto;
+  display: block;
+  text-align: center;
+  margin-left: auto;
   margin-right: auto;
-width:30%;
-text-decoration:none;
-  
-  
+  width: 27%;
+  text-decoration: none;
+  background: linear-gradient(white, white) padding-box,
+    linear-gradient(to right, darkblue, darkorchid) border-box;
+  border-radius: 50em;
+  border: 4px solid transparent;
 `;
 
 export default Footer;
